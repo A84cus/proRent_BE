15fb1e484351da8031042accfc545fc9361d476c
Adi Nurfatwa <AdiNurfatwa@gmail.com>
2025-08-07 13:57:57 +0700

    feat: add reservation query feature

--- dist/route/reservationRoute.js
+++ dist/route/reservationRoute.js
@@ -8,9 +8,14 @@ const reservationController_1 = require("../controller/reservationController/res
 const paymentProofController_1 = require("../controller/reservationController/paymentProofController");
 const authMwr_1 = require("../middleware/authMwr");
 const uploader_1 = require("../utils/uploader");
+const reservationQueryController_1 = require("../controller/reservationController/reservationQueryController");
 const router = express_1.default.Router();
 const uploadFile = (0, uploader_1.memoryUploader)().single('file');
 // Reservation routes
+router.get('/', reservationQueryController_1.getReservations); // General query
+router.get('/user/:userId?', reservationQueryController_1.getUserReservationsHandler); // For a specific user or current user
+router.get('/tenant/:propertyOwnerId?', reservationQueryController_1.getTenantReservationsHandler); // Property owner reservations
+router.get('/property/:propertyId', reservationQueryController_1.getPropertyReservationsHandler); // Reservations for a specific property
 // POST /reservation - Create a new reservation
 router.post('/', authMwr_1.authUser, reservationController_1.createReservationController);
 router.post('/:reservationId/cancel', authMwr_1.authUser, reservationController_1.cancelReservationController);
--- src/route/reservationRoute.ts
+++ src/route/reservationRoute.ts
@@ -3,16 +3,26 @@ import {
    createReservationController,
    cancelReservationController
 } from '../controller/reservationController/reservationController';
+
 import { uploadPayment } from '../controller/reservationController/paymentProofController';
 import { authUser } from '../middleware/authMwr';
 import { multipleFileDiffField, multipleFileSameField, memoryUploader, validateImageFile } from '../utils/uploader';
+import {
+   getPropertyReservationsHandler,
+   getReservations,
+   getTenantReservationsHandler,
+   getUserReservationsHandler
+} from '../controller/reservationController/reservationQueryController';
 
 const router = express.Router();
 
 const uploadFile = memoryUploader().single('file');
 
 // Reservation routes
-
+router.get('/', getReservations); // General query
+router.get('/user/:userId?', getUserReservationsHandler); // For a specific user or current user
+router.get('/tenant/:propertyOwnerId?', getTenantReservationsHandler); // Property owner reservations
+router.get('/property/:propertyId', getPropertyReservationsHandler); // Reservations for a specific property
 // POST /reservation - Create a new reservation
 router.post('/', authUser, createReservationController);
 router.post('/:reservationId/cancel', authUser, cancelReservationController);
