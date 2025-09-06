1d190c5bf354548263de7ac6c28751b729907b5a
Adi Nurfatwa <AdiNurfatwa@gmail.com>
2025-09-07 06:47:14 +0700

    fix: update xendit callback routes

--- dist/route/webhooks/xenditRoute.js
+++ dist/route/webhooks/xenditRoute.js
@@ -9,5 +9,5 @@ const controller_1 = require("../../controller"); // Adjust path if needed
 const router = express_1.default.Router();
 // Apply express.raw() middleware ONLY to the Xendit webhook route
 // This is crucial for verifying the Xendit-Signature
-router.post('/invoice', express_1.default.raw({ type: 'application/json' }), controller_1.handleXenditInvoiceCallback);
+router.post('/xendit/invoice', express_1.default.raw({ type: 'application/json' }), controller_1.handleXenditInvoiceCallback);
 exports.default = router;
